c4b2053d4646a073642eec35fb6d29bfd7abb7cb
Alexander Yeliseyev <34523832+boltikpravyi@users.noreply.github.com>
2024-10-13 01:19:21 +0300

    Add files via upload

---
+++ .Rprofile
@@ -0,0 +1,13 @@
+# Load packages
+library(readxl) # import excel files
+library(tidyverse) # data science packages
+library(AER) # censored regression model
+library(crch) # censored regression w/ heteroscedastic errors
+library(MatchIt) # matching algorithms
+library(sandwich) # robust standard errors
+library(lmtest) # tests for estimated coefficients
+library(pcalg) # Peter-Clark causal discovery algorithm
+library(modelsummary) # representation of models' results
+library(marginaleffects) # calculate marginal effects from models
+library(knitr) # dynamic report generation
+library(Rgraphviz) # plot graphs
--- main_script.R
+++ main_script.R
@@ -1,16 +1,4 @@
-# Load packages
-# library(readxl)
-# library(tidyverse)
-# # library(caret)
-# # library(psych)
-# # library(stargazer)
-# library(censReg)
-# # library(AER)
-# # library(rpart)
-# # library(rpart.plot)
-# library(MatchIt) # propensity score matching
-# library(sandwich) # heterokedasticity robust standard errors
-# library(lmtest) # coefficient testing
+# 0. Preliminary steps -------------------------------------------------------------------
 
 # Load data
 rawdata <- read_excel(path = 'data.xlsx')
@@ -18,137 +6,64 @@ rawdata <- read_excel(path = 'data.xlsx')
 # Transform data
 df <- rawdata %>% 
   rename(ID = 'ID пациента',
-         Tr = 'T') %>% 
-  mutate(Yr = round(Y, 2)) %>% # for censreg estimates
-  # mutate(Xr = round(X, 2)) %>% # for PS estimates
-  mutate(Xc = X - mean(X)) %>% # NB! X*Tr interaction term always improves ATE precion estimate (from Causal ML book)
-  # arrange(Y) %>% 
-  # mutate(Treatment = as_factor(Treatment)) #%>% 
-  mutate(Label = as_factor(Tr)) #%>%
-  # mutate(Label = case_when(Treatment == 0 ~ 'P', Treatment == 1 ~ 'T')) %>% 
-  # mutate(Label = as_factor(Label))
+         D = 'T') %>% 
+  mutate(Yr = round(Y, 2)) %>% # for censored regression estimates
+  mutate(Label = as_factor(D)) %>%
+  mutate(X2 = X^2, X3 = X^3, X4 = X^4, X5 = X^5, X6 = X^6) %>% 
+  mutate(Xc = X - mean(X), X2c = X2 - mean(X2), X3c = X3 - mean(X3), 
+         X4c = X4 - mean(X4), X5c = X5 - mean(X5), X6c = X6 - mean(X6))
+
+# Matching treatment and control
+estMatch <- matchit(D ~ Xc, data = df, method = 'nearest') # 1:1 greedy NN on the PS
+dfMatch <- (df %>% dplyr::filter(ID %in% as.numeric(estMatch$match.matrix[,1]))) %>%
+  bind_rows(df %>% dplyr::filter(D == 1)) %>% 
+  mutate(X2 = X^2, X3 = X^3, X4 = X^4, X5 = X^5, X6 = X^6) %>% 
+  mutate(Xc = X - mean(X), X2c = X2 - mean(X2), X3c = X3 - mean(X3), 
+         X4c = X4 - mean(X4), X5c = X5 - mean(X5), X6c = X6 - mean(X6))
 
 # 1. Exploratory data analysis -----------------------------------------------------------
 
-# Observed outcome across the patients
+# Number of treated (1) and untreated (0)
 df %>% 
-  ggplot(mapping = aes(x = ID, y = Y, color = Label)) +
-  geom_point() +
-  theme_bw() +
-  theme(legend.position = 'top') +
-  labs(x = 'Patient', y = 'Observed health outcome') +
-  scale_color_discrete(name = '', labels = c('Placebo', 'Treatment'))
+  group_by(D) %>% 
+  summarise(Y = n())
 
-# Distribution of observed outcome
+# Distribution of observed outcomes
 df %>% 
   ggplot(mapping = aes(x = Y, fill = Label)) +
     geom_histogram(bins = 250) +
     theme_bw() +
-    theme(legend.position = 'top') + 
-    labs(x = 'Observed health outcome', y = 'No of evidence') +
-    scale_fill_discrete(name = '', labels = c('Placebo', 'Treatment'))
-
-# Descriptive statistics
-descriptive_stats <- df %>% 
-  select(-ID) %>% 
-  group_by(Tr) %>% 
-  summarise(n = n(),
-            distinct_y = n_distinct(Y),
-            mean_y = mean(Y), sd_y = sd(Y),
-            min_y = min(Y), max_y = max(Y),
-            median_y = median(Y),
-            distinct_x = n_distinct(X),
-            mean_x = mean(X), sd_x = sd(X),
-            min_x = min(X), max_x = max(X),
-            median_x = median(X))
-descriptive_stats %>% 
-  select(Tr, n, mean_y, sd_y, min_y, max_y, median_y)
-descriptive_stats %>% 
-  select(Tr, n, mean_x, sd_x, min_x, max_x, median_x)
-
-# Data on pre-treatment patients' characteristics
-df %>% 
-  ggplot(mapping = aes(x = X, fill = Label)) +
-  geom_histogram(aes(y = ..density..), position = 'identity', alpha = 0.33, bins = 50) +
-  theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Pre-treatment K steps per day', y = 'Density') +
-  scale_fill_discrete(name = '', labels = c('Placebo', 'Treatment'))
-
-# No statistical difference between treated and control group characteristics
-lm(data = df, formula = X ~ Tr) %>% summary()
-
-# How observed outcome is related to pre-treatment patients' characteristics
-df %>% 
-  ggplot(mapping = aes(x = X, y = Y, color = Label)) +
-  geom_point() +
-  stat_smooth(method = 'lm', formula = y ~ x, geom = 'smooth') +
-  theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Pre-treatment K steps per day', y = 'Observed health outcome') +
-  scale_color_discrete(name = '', labels = c('Placebo', 'Treatment'))
-
-# Association between observed outcome and pre-treatment patients' characteristics
-lm(Y ~ X, data = df) %>% summary()
-
-# 2. Estimation of ATE -------------------------------------------------------------------
-
-# OLS-based estimator based on linear regression (= Diff-in-means estimator)
-estOLS <- lm(Y ~ Tr, data = df)
-# estOLS <- lm(Y ~ Tr + Xc*Tr, data = df)
-summary(estOLS)
-# confint(estOLS) # NB! Use standard errors robusted to misspec, hc and ac (from Causal ML book)
-# Heteroskedasticity of error term
-resOLS <- estOLS$residuals
-plot(y = resOLS^2, x = df$Tr)
-plot(y = resOLS^2, x = df$Xc)
-lm(resOLS^2 ~ df$Tr + df$Xc) %>% summary() # significant presence of hc
-coeftest(estOLS, vcov = vcovHC(estOLS, type = 'HC1')) # hc robust inference (Eicker-Huber-White)
-summary(estOLS) # classical (non-robust) inference
-
-# Censored regression estimator (removes downward bias of OLS estimator)
-estCens <- censReg(Yr ~ Tr, data = df, left = min(df$Yr), right = max(df$Yr))
-# estCens <- censReg(Yr ~ Tr + Xc*Tr, data = df, left = min(df$Yr), right = max(df$Yr))
-summary(estCens)
-margEff(estCens) # Marginal effect is different from coeficient estimate
-confint(estCens) # NB! Use standard errors robusted to misspec, hc and ac (from Causal ML book)
-# NB! Add delta method for correct s.e.
-# coeftest(estCens, vcov = vcovHC(estCens, type = 'HC1')) # hc robust inference (Eicker-Huber-White)
-
-# Matching treatment and control
-estMatch <- matchit(Tr ~ Xc, data = df, method = 'nearest') # 1:1 greedy NN on the PS
-summary(estMatch)
-dfMatch <- (df %>% 
-  filter(ID %in% as.numeric(estMatch$match.matrix[,1]))) %>% 
-  bind_rows(
-    df %>% 
-      filter(Tr == 1)
-  )
-
-# EDA for matched observations ----
-
-# Observed outcome across the patients
-dfMatch %>% 
-  ggplot(mapping = aes(x = ID, y = Y, color = Label)) +
-  geom_point() +
-  theme_bw() +
-  theme(legend.position = 'top') +
-  labs(x = 'Patient', y = 'Observed health outcome') +
-  scale_color_discrete(name = '', labels = c('Placebo', 'Treatment'))
-
-# Distribution of observed outcome
+    theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+    labs(x = 'Observed health outcome (Y)', y = 'No of evidence', title = 'No matching') +
+    scale_fill_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
 dfMatch %>% 
   ggplot(mapping = aes(x = Y, fill = Label)) +
   geom_histogram(bins = 250) +
   theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Observed health outcome', y = 'No of evidence') +
-  scale_fill_discrete(name = '', labels = c('Placebo', 'Treatment'))
+  theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Observed health outcome (Y)', y = 'No of evidence', title = 'Exact matching') +
+  scale_fill_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
 
 # Descriptive statistics
-descriptive_stats <- dfMatch %>% 
+desc <- df %>% 
   select(-ID) %>% 
-  group_by(Tr) %>% 
+  group_by(D) %>% 
+  summarise(n = n(),
+            distinct_y = n_distinct(Y),
+            mean_y = mean(Y), sd_y = sd(Y),
+            min_y = min(Y), max_y = max(Y),
+            median_y = median(Y),
+            distinct_x = n_distinct(X),
+            mean_x = mean(X), sd_x = sd(X),
+            min_x = min(X), max_x = max(X),
+            median_x = median(Xc))
+desc %>% 
+  select(D, n, mean_y, sd_y, min_y, max_y, median_y)
+desc %>% 
+  select(D, n, mean_x, sd_x, min_x, max_x, median_x)
+descMatch <- dfMatch %>% 
+  select(-ID) %>% 
+  group_by(D) %>% 
   summarise(n = n(),
             distinct_y = n_distinct(Y),
             mean_y = mean(Y), sd_y = sd(Y),
@@ -158,60 +73,124 @@ descriptive_stats <- dfMatch %>%
             mean_x = mean(X), sd_x = sd(X),
             min_x = min(X), max_x = max(X),
             median_x = median(X))
-descriptive_stats %>% 
-  select(Tr, n, mean_y, sd_y, min_y, max_y, median_y)
-descriptive_stats %>% 
-  select(Tr, n, mean_x, sd_x, min_x, max_x, median_x)
+descMatch %>% 
+  select(D, n, mean_y, sd_y, min_y, max_y, median_y)
+descMatch %>% 
+  select(D, n, mean_x, sd_x, min_x, max_x, median_x)
 
-# Data on pre-treatment patients' characteristics
+# Pre-treatment patients' characteristics
+df %>% 
+  ggplot(mapping = aes(x = X, fill = Label)) +
+  geom_histogram(aes(y = after_stat(density)), position = 'identity', alpha = 0.33, bins = 50) +
+  theme_bw() +
+  theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Pre-treatment thousands of steps per day (X)', y = 'Density', title = 'No matching') +
+  scale_fill_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
 dfMatch %>% 
   ggplot(mapping = aes(x = X, fill = Label)) +
-  geom_histogram(aes(y = ..density..), position = 'identity', alpha = 0.33, bins = 50) +
+  geom_histogram(aes(y = after_stat(density)), position = 'identity', alpha = 0.33, bins = 50) +
   theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Pre-treatment K steps per day', y = 'Density') +
-  scale_fill_discrete(name = '', labels = c('Placebo', 'Treatment'))
+  theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Pre-treatment thousands of steps per day (X)', y = 'Density', title = 'Exact matching') +
+  scale_fill_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
 
 # No statistical difference between treated and control group characteristics
-lm(X ~ Tr, data = dfMatch) %>% summary()
+lm(D ~ X, data = df) %>% 
+  coeftest(vcov = vcovHC(., type = 'HC3')) # hc-consistent s.e. for robust statistical inference
+lm(D ~ X, data = dfMatch) %>%
+  coeftest(vcov = vcovHC(., type = 'HC3'))
 
 # How observed outcome is related to pre-treatment patients' characteristics
+df %>% 
+  ggplot(mapping = aes(x = X, y = Y, color = Label)) +
+  geom_point() +
+  stat_smooth(method = 'lm', formula = y ~ x, geom = 'smooth') +
+  theme_bw() +
+  theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Pre-treatment thousands of steps per day (X)', y = 'Observed health outcome (Y)', title = 'No matching') +
+  scale_color_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
 dfMatch %>% 
   ggplot(mapping = aes(x = X, y = Y, color = Label)) +
   geom_point() +
   stat_smooth(method = 'lm', formula = y ~ x, geom = 'smooth') +
   theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Pre-treatment K steps per day', y = 'Observed health outcome') +
-  scale_color_discrete(name = '', labels = c('Placebo', 'Treatment'))
+  theme(legend.position = 'top', plot.title = element_text(hjust = 0.5)) + 
+  labs(x = 'Pre-treatment thousands of steps per day (X)', y = 'Observed health outcome (Y)', title = 'Exact matching') +
+  scale_color_discrete(name = '', labels = c('Control (D=0)', 'Treatment (D=1)'))
+  
+# Make causal discovery procedure to understand connections between variables
+suff_stat <- list(C = cor(df[,2:4]), n = nrow(df[,2:4]))
+estPC <- pc(suff_stat, indepTest = gaussCItest, labels = colnames(df[,2:4]), alpha = 0.01, skel.method = 'stable.fast')
+plot(estPC, main = 'Directed acyclic graph (based on PC algorithm)')
 
-# Association between observed outcome and pre-treatment patients' characteristics
-lm(Y ~ X, data = dfMatch) %>% summary()
+# 2. Estimation of ATE -------------------------------------------------------------------
 
-# OLS estimator w/ matched observations
-estOLSMatch <- lm(Y ~ Tr, data = dfMatch)
-# estOLSMatch <- lm(Y ~ Tr + Xc*Tr, data = dfMatch)
-summary(estOLSMatch)
-confint(estOLSMatch) # NB! Use standard errors robusted to misspec, hc and ac (from Causal ML book)
-# Heteroskedasticity of error term
-resOLSMatch <- estOLSMatch$residuals
-plot(y = resOLSMatch^2, x = dfMatch$Tr)
-plot(y = resOLSMatch^2, x = dfMatch$Xc)
-lm(resOLSMatch^2 ~ dfMatch$Tr + dfMatch$Xc) %>% summary() # significant presence of hc
+# OLS estimators (no matching)
+estOLS <- lm(Y ~ D, data = df) # Diff-in-means estimator
+summary(estOLS)
+coeftest(estOLS, vcov = vcovHC(estOLS, type = 'HC3')) # hc robust inference (Eicker-Huber-White)
+mefOLS <- avg_slopes(estOLS, variables = 'D', vcov = 'HC3')
+mefOLS
+estOLSControl <- lm(Y ~ D + Xc*D + X2c*D + X3c*D, data = df) # Interaction term always improves the precision of ATE
+coeftest(estOLSControl, vcov = vcovHC(estOLSControl, type = 'HC3')) # hc robust inference (Eicker-Huber-White)
+mefOLSControl <- avg_slopes(estOLSControl, variables = 'D', vcov = 'HC3')
+mefOLSControl
 
-# Censored regression estimator w/ matched observations
-estCensMatch <- censReg(Yr ~ Tr, data = dfMatch, left = min(dfMatch$Yr), right = max(dfMatch$Yr))
-# estCensMatch <- censReg(Yr ~ Tr + Xc*Tr, data = dfMatch, left = min(dfMatch$Yr), right = max(dfMatch$Yr))
+# OLS estimators (exact matching)
+estOLSMatch <- lm(Y ~ D, data = dfMatch)
+coeftest(estOLSMatch, vcov = vcovHC(estOLSMatch, type = 'HC3'))
+mefOLSMatch <- avg_slopes(estOLSMatch, variables = 'D', vcov = 'HC3')
+mefOLSMatch
+estOLSMatchControl <- lm(Y ~ D + Xc*D + X2c*D + X3c*D, data = dfMatch)
+coeftest(estOLSMatchControl, vcov = vcovHC(estOLSMatchControl, type = 'HC3'))
+mefOLSMatchControl <- avg_slopes(estOLSMatchControl, variables = 'D', vcov = 'HC3')
+mefOLSMatchControl
+
+# Censored ML estimators (no matching)
+estCens <- tobit(Yr ~ D, data = df, left = min(df$Yr), right = max(df$Yr), robust = T)
+summary(estCens)
+mefCens <- avg_slopes(estCens, variables = 'D')
+mefCens
+estCensControl <- tobit(Yr ~ D + Xc*D + X2c*D + X3c*D, data = df, left = min(df$Yr), right = max(df$Yr), robust = T)
+summary(estCensControl)
+mefCensControl <- avg_slopes(estCensControl, variables = 'D')
+mefCensControl
+
+# Censored ML estimators (exact matching)
+estCensMatch <- tobit(Yr ~ D, data = dfMatch, left = min(dfMatch$Yr), right = max(dfMatch$Yr), robust = T)
 summary(estCensMatch)
-margEff(estCensMatch) # Marginal effect is different from coeficient estimate
-confint(estCensMatch) # NB! Use standard errors robusted to misspec, hc and ac (from Causal ML book)
-# NB! Add delta method for correct s.e.
+mefCensMatch <- avg_slopes(estCensMatch, variables = 'D')
+mefCensMatch
+estCensMatchControl <- tobit(Yr ~ D + Xc*D + X2c*D + X3c*D, data = dfMatch, left = min(dfMatch$Yr), right = max(dfMatch$Yr), robust = T)
+summary(estCensMatchControl)
+mefCensMatchControl <- avg_slopes(estCensMatchControl, variables = 'D')
+mefCensMatchControl
 
-# Estimates of ATE and 95% confidence interval
-ATEest <- tibble(OLS = coef(estOLS)[2], CensReg = margEff(estCens)[1],
-                 OLSMatch = coef(estOLSMatch)[2], CensRegMatch = margEff(estCensMatch)[1])
-ATEconfint <- tibble(OLS = confint(estOLS)[2,], CensReg = confint(estCens)[2,],
-                     OLSMatch = confint(estOLSMatch)[2,], CensRegMatch = confint(estCensMatch)[2,])
-ATEprec <- ATEconfint %>% 
-  mutate(OLS = diff(OLS), CensReg = diff(CensReg),
-         OLSMatch = diff(OLSMatch), CensRegMatch = diff(CensRegMatch)) %>% .[1,]
+# Censored ML Estimators w/ heteroscedastic errors
+estCensHC <- crch(Yr ~ D | D + Xc + X2c + X3c + X4c, data = df, left = min(df$Yr), right = max(df$Yr))
+summary(estCensHC)
+mefCensHC <- avg_slopes(estCensHC, variables = 'D')
+mefCensHC
+estCensHCMatch <- crch(Yr ~ D | D + Xc + X2c + X3c + X4c, data = dfMatch, left = min(dfMatch$Yr), right = max(dfMatch$Yr))
+summary(estCensHCMatch)
+mefCensHCMatch <- avg_slopes(estCensHCMatch, variables = 'D')
+mefCensHCMatch
+
+# 3. Results -----------------------------------------------------------------------------
+
+mefs <- list(
+  'Panel A: No matching' = list(
+    'OLS' = mefOLS,
+    'OLS w/ X' = mefOLSControl,
+    'CR' = mefCens,
+    'CR w/ X' = mefCensControl,
+    'CR w/ het. err.' = mefCensHC),
+  'Panel B: Exact matching' = list(
+    'OLS' = mefOLSMatch,
+    'OLS w/ X' = mefOLSMatchControl,
+    'CR' = mefCensMatch,
+    'CR w/ X' = mefCensMatchControl,
+    'CR w/ het. err.' = mefCensHCMatch))
+modelsummary(mefs, shape = 'rbind', stars = T, statistic = c('s.e. = {std.error}', 'conf.int'),
+             coef_rename = c('D' = 'PATE, point estimate'), conf_level = .99,
+             gof_omit = c('IC|Log|R2|F|Std|r2'))
